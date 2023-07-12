0db79e6bed06661575a5847e4d2dbcfc6bf2d112
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-07-12 11:25:53 +0200

    Add release bullet to manually review WORDLIST

--- R/dev-utils.R
+++ R/dev-utils.R
@@ -1,8 +1,9 @@
 # This unexported function adds a custom item to `usethis::use_release_issue()`
 release_bullets <- function() {
 
-  c( # nolint
-    "Run `goodpractice::gp()`"
+  c(
+    "Run `goodpractice::gp()`",
+    "Review [WORDLIST](https://docs.cran.dev/spelling#wordlist)"
   )
 
 }
