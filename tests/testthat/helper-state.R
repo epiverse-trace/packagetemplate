9cf5e17f33ea95781b852cfaf5feafc3046ce402
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-11-29 15:59:54 +0100

    Add test on R version to state inspector

--- tests/testthat/helper-state.R
+++ tests/testthat/helper-state.R
@@ -3,20 +3,24 @@
 # If global settings need to be modified, they should be restored to their
 # original values on exit. This can be achieved with the `on.exit()` base
 # function, or more conveniently with the `withr` package.
-testthat::set_state_inspector(function() {
-  list(
-    attached    = search(),
-    connections = getAllConnections(),
-    cwd         = getwd(),
-    envvars     = Sys.getenv(),
-    handlers    = globalCallingHandlers(),
-    libpaths    = .libPaths(),
-    locale      = Sys.getlocale(),
-    options     = options(),
-    par         = par(),
-    packages    = .packages(all.available = TRUE),
-    sink        = sink.number(),
-    timezone    = Sys.timezone(),
-    NULL
-  )
-})
+# We add a test on R >= 4.0.0 because some functions such as
+# `globalCallingHandlers()` did not exist before.
+if (getRversion() >= "4.0.0") {
+  testthat::set_state_inspector(function() {
+    list(
+      attached    = search(),
+      connections = getAllConnections(),
+      cwd         = getwd(),
+      envvars     = Sys.getenv(),
+      handlers    = globalCallingHandlers(),
+      libpaths    = .libPaths(),
+      locale      = Sys.getlocale(),
+      options     = options(),
+      par         = par(),
+      packages    = .packages(all.available = TRUE),
+      sink        = sink.number(),
+      timezone    = Sys.timezone(),
+      NULL
+    )
+  })
+}
