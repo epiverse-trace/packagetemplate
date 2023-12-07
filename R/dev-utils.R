cac8c179db86529825237fc6ec95646b9c206324
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-12-07 10:38:37 +0100

    Add release note to ensure contributors are acknowledged Fix #89

--- R/dev-utils.R
+++ R/dev-utils.R
@@ -4,7 +4,8 @@ release_bullets <- function() {
   c(
     "Run `goodpractice::gp()`",
     "Review [WORDLIST](https://docs.cran.dev/spelling#wordlist)",
-    "Check if `# nolint` comments are still needed with recent lintr releases"
+    "Check if `# nolint` comments are still needed with recent lintr releases",
+    "All contributors to this release are acknowledged in some way"
   )
 
 }
