2b5af2fe6d228cadabf4e786c41c0ec9aba74389
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-12-05 12:32:31 +0100

    Only inspect a subset of par() in helper-state (#166)

--- tests/testthat/helper-state.R
+++ tests/testthat/helper-state.R
@@ -5,6 +5,15 @@
 # function, or more conveniently with the `withr` package.
 # We add a test on R >= 4.0.0 because some functions such as
 # `globalCallingHandlers()` did not exist before.
+get_pars_toreset <- function() {
+  pars <- par(no.readonly = TRUE)
+  # The following params are set and modified automatically by plot(), as
+  # documented in ?plot.window() and we:
+  # 1. have no control over them
+  # 2. do not care about resetting them
+  pars <- pars[!names(pars) %in% c("usr", "xaxp", "yaxp")]
+}
+
 if (getRversion() >= "4.0.0") {
   testthat::set_state_inspector(function() {
     list(
@@ -16,7 +25,7 @@ if (getRversion() >= "4.0.0") {
       libpaths    = .libPaths(),
       locale      = Sys.getlocale(),
       options     = options(),
-      par         = par(),
+      par         = get_pars_toreset(),
       packages    = .packages(all.available = TRUE),
       sink        = sink.number(),
       timezone    = Sys.timezone(),
